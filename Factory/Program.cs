abb1a4dd655ca0993298549a69f513c183ebbebd
Thomas Bakken <tbakken14@gmail.com>
2023-08-04 10:06:07 -0700

    Initial file structure

---
+++ Factory/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<UniversityRegistrarContext>(
+                dbContextOptions => dbContextOptions.UseMySql(
+                    builder.Configuration["ConnectionStrings:DefaultConnection"],
+                    ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"])));
+
+            WebApplication app = builder.Build();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+            );
+
+            app.Run();
+        }
+    }
+}
